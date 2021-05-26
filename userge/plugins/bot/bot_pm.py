eae51751ab91f7453efaaba1105309082278fcc5
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-05-26 16:25:46 -0300

    update bot_pm.py

--- userge/plugins/bot/bot_pm.py
+++ userge/plugins/bot/bot_pm.py
@@ -70,9 +70,9 @@ if userge.has_bot:
             return bool(
                 (m.chat and m.chat.type == "private") and m.matches and not m.edit_date
             )
-
+          
         return filters.create(func, "StartFilter")
-
+        
     def drive_filter() -> filters:
         async def funcc(_, __, m: Message) -> bool:
             textt = m.text or m.caption
@@ -176,7 +176,7 @@ if userge.has_bot:
                     CHANNEL.log(bot_start_msg),
                 )
         return not bool(found)
-
+        
     async def check_new_bot_userr(user: Union[int, str, User]) -> bool:
         userr_ = await userge.bot.get_user_dict(user, attr_dict=True)
         if userr_.id in Config.OWNER_ID:
@@ -253,11 +253,11 @@ My Master is: {owner_.flname}</b>
                 f"**ERROR**: {str(bpm_e)}\n\nFatal Error occured while sending Bot Pm Media"
             )
         await check_new_bot_user(message.from_user)
-
+        
     @userge.bot.on_message(drive_filter())
     async def drive_bot(_, message: Message):
         c_infoo = await get_bot_info()
-        c_infoo.get("bot")
+        bott_ = c_infoo.get("bot")
         ownerr_ = c_infoo.get("owner")
         from_userr = await userge.bot.get_user_dict(message.from_user, attr_dict=True)
         if from_userr.id in Config.OWNER_ID:
@@ -275,7 +275,7 @@ https://drive.google.com/file/d/1OGN-8kUoP0OPsCEp3zlfaPu94EzsrZ9z/view?usp=drive
             )
             btnss = [
                 [
-                    InlineKeyboardButton("👤  CONTACT", url=contact_url),
+                    InlineKeyboardButton("👤  CONTACT", url=contact_urll),
                     InlineKeyboardButton("⭐️  REPO", url=Config.UPSTREAM_REPO),
                 ]
             ]
@@ -285,8 +285,8 @@ https://drive.google.com/file/d/1OGN-8kUoP0OPsCEp3zlfaPu94EzsrZ9z/view?usp=drive
                 await asyncio.sleep(e.x + 10)
             except Exception as bpm_e:
                 await CHANNEL.log(
-                    f"**ERROR**: {str(bpm_e)}\n\nFatal Error occured while sending Bot Pm Media"
-                )
+                f"**ERROR**: {str(bpm_e)}\n\nFatal Error occured while sending Bot Pm Media"
+            )
         await check_new_bot_userr(message.from_user)
 
     @userge.bot.on_callback_query(filters.regex(pattern=r"^add_to_grp$"))
@@ -307,11 +307,11 @@ https://drive.google.com/file/d/1OGN-8kUoP0OPsCEp3zlfaPu94EzsrZ9z/view?usp=drive
     @check_owner
     async def back_bot_pmm_(c_q: CallbackQuery):
         await c_q.answer()
-        (drive_msg,) = default_owner_drive(
+        drive_msg,  = default_owner_drive(
             await userge.bot.get_user_dict(c_q.from_user, attr_dict=True)
         )
         await c_q.edit_message_text(drive_msg, reply_markup=InlineKeyboardMarkup())
-
+        
     @userge.bot.on_callback_query(filters.regex(pattern=r"^back_bot_pm$"))
     @check_owner
     async def back_bot_pm_(c_q: CallbackQuery):
