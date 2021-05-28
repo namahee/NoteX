6e8c3b8a34d0fa8a73a4fb173b9da2d8553fdf54
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-05-28 14:46:41 -0300

    update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -229,7 +229,7 @@ class Bot_Alive:
         alive_info_ = f"""
 <a href="https://telegram.dog/x_xtests"><b>NoteX</a> is Up and Running.</b>
 
-  🐍   <b>Pytho :</b>    <code>v{versions.__python_version__}</code>
+  🐍   <b>Python :</b>    <code>v{versions.__python_version__}</code>
   🔥   <b>Pyrogram :</b>    <code>v{versions.__pyro_version__}</code>
   🧬   <b>𝑿 :</b>    <code>v{get_version()}</code>
 
@@ -261,7 +261,7 @@ class Bot_Alive:
             "https://telegra.ph/file/56e32005fdc92cd3b1fa1.jpg",
             "https://telegra.ph/file/40124fa6a893c1e5cc9d0.jpg",
             "https://telegra.ph/file/de8ea1e99b99ae17fd44d.jpg",
-            "https://telegra.ph/file/b0d34b6b2cdc379dd2d19.jpg",
+            "https://telegra.ph/file/b0d34b6b2cdc379dd2d19.jpg"
         ]
         return rand_array(alive_imgs)
 
