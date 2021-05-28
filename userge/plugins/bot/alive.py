8c960751f52e3d06d20c051f700c0eb3785fa651
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-05-28 14:44:58 -0300

    update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -258,11 +258,10 @@ class Bot_Alive:
     @staticmethod
     def alive_default_imgs() -> str:
         alive_imgs = [
-            "https://telegra.ph/file/11123ef7dff2f1e19e79d.jpg",
-            "https://i.imgur.com/uzKdTXG.jpg",
-            "https://telegra.ph/file/6ecab390e4974c74c3764.png",
-            "https://telegra.ph/file/995c75983a6c0e4499b55.png",
-            "https://telegra.ph/file/86cc25c78ad667ca5e691.png",
+            "https://telegra.ph/file/56e32005fdc92cd3b1fa1.jpg",
+            "https://telegra.ph/file/40124fa6a893c1e5cc9d0.jpg",
+            "https://telegra.ph/file/de8ea1e99b99ae17fd44d.jpg",
+            "https://telegra.ph/file/b0d34b6b2cdc379dd2d19.jpg"
         ]
         return rand_array(alive_imgs)
 
