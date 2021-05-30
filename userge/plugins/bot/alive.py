8e722a6dde39c1660a9b859d2257160345c56fb3
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-05-30 15:14:59 -0300

    update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -77,7 +77,7 @@ async def send_inline_alive(message: Message) -> None:
         await message.err(str(ex), del_in=5)
         return
     await message.delete()
-    await asyncio.sleep(200)
+    await asyncio.sleep(60)
     await userge.delete_messages(message.chat.id, i_res_id)
 
 
@@ -261,7 +261,7 @@ class Bot_Alive:
             "https://telegra.ph/file/56e32005fdc92cd3b1fa1.jpg",
             "https://telegra.ph/file/40124fa6a893c1e5cc9d0.jpg",
             "https://telegra.ph/file/de8ea1e99b99ae17fd44d.jpg",
-            "https://telegra.ph/file/b0d34b6b2cdc379dd2d19.jpg",
+            "https://telegra.ph/file/b0d34b6b2cdc379dd2d19.jpg"
         ]
         return rand_array(alive_imgs)
 
