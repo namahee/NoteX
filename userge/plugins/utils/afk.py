beb01916dc136e8d672735188146252b76432f53
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-05-30 01:02:51 +0000

    auto: black code format

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -107,7 +107,6 @@ async def handle_afk_incomming(message: Message) -> None:
             out_str = (
                 f"I'm **AFK** right now.\nReason: <code>[{REASON}]({LINK})</code>\n"
                 f"Last Seen: `{afk_time} ago`"
-                
             )
         else:
             out_str = choice(AFK_REASONS)
