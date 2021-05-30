bcd450fbb7376867040edb27f0eb89c157c5650a
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-05-30 16:52:44 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -46,7 +46,7 @@ async def active_afk(message: Message) -> None:
     global REASON, LINK, IS_AFK, TIME  # pylint: disable=global-statement
     IS_AFK = True
     TIME = time.time()
-    REASON =message.input_str.split("|", maxsplit=1)
+    REASON = message.input_str.split("|", maxsplit=1)
     LINK = message.input_str
     await asyncio.gather(
         CHANNEL.log(f"You went AFK! : `{REASON}`"),
