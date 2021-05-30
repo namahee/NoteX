584a4d21b184b3c8b7db00c899b0beaa2c01be15
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-05-29 23:07:07 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -106,7 +106,7 @@ async def handle_afk_incomming(message: Message) -> None:
         if REASON or LINK:
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: <code>{REASON}</code>\n"
-                f"Last Seen: `{afk_time}` ago \n",
+                f"Last Seen: `{afk_time}` ago"
                 choice(LINKS),
             )
         else:
@@ -229,7 +229,8 @@ I'll get back to you later.",
     "I am not here right now...\nbut if I was...\n\nwouldn't that be awesome?",
 )
 LINKS = (
-    "[\u3164](https://telegra.ph/file/b0d34b6b2cdc379dd2d19.jpg",
-    "[\u3164](https://telegra.ph/file/de8ea1e99b99ae17fd44d.jpg)",
-    "[\u3164](https://telegra.ph/file/6ccc956f49583d3ca8db5.jpg)",
-)
+  "[\u3164](https://telegra.ph/file/b0d34b6b2cdc379dd2d19.jpg",
+  "[\u3164](https://telegra.ph/file/de8ea1e99b99ae17fd44d.jpg)",
+  "[\u3164](https://telegra.ph/file/6ccc956f49583d3ca8db5.jpg)",
+  )
+  
