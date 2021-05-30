fb8a4e1b6eee411cc7780cb0d0b70b74fe9ca53d
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-05-29 22:44:41 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -47,7 +47,7 @@ async def active_afk(message: Message) -> None:
     IS_AFK = True
     TIME = time.time()
     REASON = message.input_str
-    LINK = message.input
+    LINK = message.input_str
     await asyncio.gather(
         CHANNEL.log(f"You went AFK! : `{REASON}`"),
         message.edit("`You went AFK!`", del_in=1),
