50fcfd09c991a2ff421aaa20187d3772f08c50de
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-05-30 00:09:55 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -227,10 +227,11 @@ I'll get back to you later.",
     "Life is so short, there are so many things to do...\nI'm away doing one of them..",
     "I am not here right now...\nbut if I was...\n\nwouldn't that be awesome?",
 )
-LINKS = (
-    "[\u3164](https://telegra.ph/file/b0d34b6b2cdc379dd2d19.jpg",
-    "[\u3164](https://telegra.ph/file/de8ea1e99b99ae17fd44d.jpg)",
-    "[\u3164](https://telegra.ph/file/6ccc956f49583d3ca8db5.jpg)",
-)
 
+LINKS = (
+  "[\u3164](https://telegra.ph/file/b0d34b6b2cdc379dd2d19.jpg",
+  "[\u3164](https://telegra.ph/file/de8ea1e99b99ae17fd44d.jpg)",
+  "[\u3164](https://telegra.ph/file/6ccc956f49583d3ca8db5.jpg)",
+  )
+  
 LINKZ = choice(LINKS)
