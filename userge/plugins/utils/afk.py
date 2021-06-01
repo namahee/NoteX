7224de14322dd9c78c3518ced941f07574cdb47f
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-05-31 22:34:42 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -113,14 +113,12 @@ async def handle_afk_incomming(message: Message) -> None:
                     f"I'm **AFK** right now, leave me alone.\nReason: `{REASON}`\n"
                     f"Last Seen: `{afk_time}` ago."
                 )
-        if "|" in REASON:
+        if '|' in REASON:
+            LINK = message.input_str.split("|", maxsplit=1),
             out_str = (
-                (
-                    f"I'm **AFK** right now, leave me alone.\nReason: `{REASON}`\n"
-                    f"Last Seen: `{afk_time}` ago. ({LINK})"
-                ),
+                f"I'm **AFK** right now, leave me alone.\nReason: `{REASON}`\n"
+                f"Last Seen: `{afk_time}` ago. ({LINK})"
             )
-            LINK = message.input_str.split("|", maxsplit=1)
         else:
             if REASON:
                 out_str = (
