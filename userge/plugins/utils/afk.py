c9cc3f7c58d2233786ce951f2ff2daf2231bcd36
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-01 11:46:33 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -4,6 +4,7 @@ import asyncio
 import random
 import time
 from random import choice, randint
+from re import compile as comp_regex
 
 from userge import Config, Message, filters, get_collection, userge
 from userge.utils import time_formatter
@@ -102,7 +103,13 @@ async def handle_afk_incomming(message: Message) -> None:
         else:
             USERS[user_id][1] += 1
     else:
-        if REASON:
+        if '|' in REASON:
+            LINK = message.input_str.split("|", maxsplit=1)
+            out_str = (
+                f"I'm **AFK** right now, leave me alone.\nReason: `{REASON}`\n"
+                f"Last Seen: `{afk_time}` ago. {LINK}"
+            )
+        elif REASON:
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: `{REASON}`\n"
                 f"Last Seen: `{afk_time}` ago."
