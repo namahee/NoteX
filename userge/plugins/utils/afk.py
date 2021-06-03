4777b1a976e4cc62600a08b96f53ee7a069124dd
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-06-03 21:00:12 +0000

    auto: black code format

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -1,10 +1,8 @@
 """ setup AFK mode """
 
 import asyncio
-import random
 import time
 from random import choice, randint
-from re import compile as comp_regex
 
 from userge import Config, Message, filters, get_collection, userge
 from userge.utils import time_formatter
@@ -57,6 +55,7 @@ async def active_afk(message: Message) -> None:
         ),
     )
 
+
 @userge.on_filters(
     IS_AFK_FILTER
     & ~filters.me
@@ -76,7 +75,6 @@ async def active_afk(message: Message) -> None:
     ),
     allow_via_bot=False,
 )
-
 async def handle_afk_incomming(message: Message) -> None:
     """handle incomming messages when you afk"""
     if not message.from_user:
@@ -104,13 +102,13 @@ async def handle_afk_incomming(message: Message) -> None:
         if "|" in REASON:
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: <code>{REASON[0]}</code>\n"
-                f"Last Seen: `{afk_time}` ago. [\u3164]({REASON[1]})" 
+                f"Last Seen: `{afk_time}` ago. [\u3164]({REASON[1]})"
             )
         else:
             out_str = (
-                    f"I'm **AFK** right now, leave me alone.\nReason: <code>{REASON[0]}</code>\n"
-                    f"Last Seen: `{afk_time}` ago"
-                )
+                f"I'm **AFK** right now, leave me alone.\nReason: <code>{REASON[0]}</code>\n"
+                f"Last Seen: `{afk_time}` ago"
+            )
         coro_list.append(message.reply(out_str))
         if chat.type == "private":
             USERS[user_id] = [1, 0, user_dict["mention"]]
