85a1c0a2800c5218a8ff0401c954b80b2749ca06
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-02 23:52:13 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -4,6 +4,7 @@ import asyncio
 import random
 import time
 from random import choice, randint
+from re import compile as comp_regex
 
 from userge import Config, Message, filters, get_collection, userge
 from userge.utils import time_formatter
@@ -77,6 +78,7 @@ async def active_afk(message: Message) -> None:
     ),
     allow_via_bot=False,
 )
+
 async def handle_afk_incomming(message: Message) -> None:
     """handle incomming messages when you afk"""
     if not message.from_user:
@@ -95,21 +97,21 @@ async def handle_afk_incomming(message: Message) -> None:
                 )
             else:
                 out_str = choice(AFK_REASONS)
-        coro_list.append(message.reply(out_str))
+            coro_list.append(message.reply(out_str))
         if chat.type == "private":
             USERS[user_id][0] += 1
         else:
             USERS[user_id][1] += 1
     else:
-        if "|" not in REASON:
+        if not '|' in REASON:
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: {REASON[0]}\n"
-                f"Last Seen: `{afk_time}` ago."
+                f"Last Seen: `{afk_time}` ago." 
             )
         else:
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: {REASON[0]}\n"
-                f"Last Seen: `{afk_time}` ago."
+                f"Last Seen: `{afk_time}` ago." 
                 f"link = {REASON[1]}"
             )
         if chat.type == "private":
