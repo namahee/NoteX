cd4fec64cb8353a94b72911e55ad18c2f21467b8
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-06-04 00:53:14 +0000

    auto: black code format

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -100,16 +100,16 @@ async def handle_afk_incomming(message: Message) -> None:
         else:
             USERS[user_id][1] += 1
     else:
-        if '|' in REASON:
+        if "|" in REASON:
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: `{REASON[0]}`\n"
                 f"Last Seen: `{afk_time}` ago [\u3164]({REASON[1]})"
             )
         else:
             out_str = (
-                    f"I'm **AFK** right now, leave me alone.\nReason: `{REASON[0]}`\n"
-                    f"Last Seen: `{afk_time} ago`"
-                )
+                f"I'm **AFK** right now, leave me alone.\nReason: `{REASON[0]}`\n"
+                f"Last Seen: `{afk_time} ago`"
+            )
         coro_list.append(message.reply(out_str))
         if chat.type == "private":
             USERS[user_id] = [1, 0, user_dict["mention"]]
