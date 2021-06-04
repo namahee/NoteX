a221aca612bd18a135c48bc52ca2b28129c0ff2c
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-03 22:18:32 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -100,15 +100,15 @@ async def handle_afk_incomming(message: Message) -> None:
         else:
             USERS[user_id][1] += 1
     else:
-        if "|" in REASON:
+        if not '|' in REASON:
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: `{REASON[0]}`\n"
-                f"Last Seen: `{afk_time}` ago {REASON[1]}"
+                f"Last Seen: `{afk_time}` agooooooooooo."
             )
         else:
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: `{REASON[0]}`\n"
-                f"Last Seen: `{afk_time}` agoooo"
+                f"Last Seen: `{afk_time}` ago. {REASON[1]}"
             )
         coro_list.append(message.reply(out_str))
         if chat.type == "private":
