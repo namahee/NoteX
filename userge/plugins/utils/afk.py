5ce63171d6732eba3ec4e421dff1de49caaf1b45
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-03 21:04:33 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -101,11 +101,11 @@ async def handle_afk_incomming(message: Message) -> None:
             USERS[user_id][1] += 1
     else:
         if "|" in REASON:
+        coro_list.append(message.reply(out_str))
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: `{REASON[0]}`\n"
                 f"Last Seen: `{afk_time}` ago [\u3164]({REASON[1]})"
             )
-        coro_list.append(message.reply(out_str))
         else:
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: `{REASON[0]}`\n"
