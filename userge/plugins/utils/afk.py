6233b75d52e0db7b4d0d86485b5dce8b13de3217
samuca78 <sennasamuel341@gmail.com>
2021-06-14 18:48:54 -0300

    ready

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -127,9 +127,9 @@ async def handle_afk_incomming(message: Message) -> None:
         else:
             USERS[user_id][1] += 1
     else:
-        r = TL.search(reason)
         match = _TELE_REGEX.search(reason)
         if match:
+            r = TL.search(reason)
             REASON = reason.replace(r.group(0), "")
             out_str = (
                 f"I'm **AFK** right now, leave me alone.\nReason: {REASON}\n"
