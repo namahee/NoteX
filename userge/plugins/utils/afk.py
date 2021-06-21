c84ee8846c3ad078f0629ff855d771de08e93f99
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-21 15:01:26 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -5,8 +5,6 @@ import time
 from random import randint
 from re import compile as comp_regex
 
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
 from userge import Config, Message, filters, get_collection, userge
 from userge.utils import time_formatter
 
@@ -14,6 +12,9 @@ _TELE_REGEX = comp_regex(
     r"http[s]?://(telegra\.ph/file|t\.me)/(\w+)(?:\.|/)(gif|jpg|png|jpeg|mp4|[0-9]+)(?:/([0-9]+))?"
 )
 TL = comp_regex(r"[<].*[>]")
+
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 
 CHANNEL = userge.getCLogger(__name__)
@@ -100,16 +101,16 @@ async def active_afk(message: Message) -> None:
     ),
     allow_via_bot=False,
 )
+
+
 def afk_buttons() -> InlineKeyboardMarkup:
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="🔧  CONTACT", callback_data="https://t.me/NoteZV"
-            ),
-            InlineKeyboardButton(text="⚡  REPO", url=Config.UPSTREAM_REPO),
+        buttons = [
+            [
+                InlineKeyboardButton(text="🔧  CONTACT", callback_data="https://t.me/NoteZV"),
+                InlineKeyboardButton(text="⚡  REPO", url=Config.UPSTREAM_REPO),
+            ]
         ]
-    ]
-    return InlineKeyboardMarkup(buttons)
+        return InlineKeyboardMarkup(buttons)
 
 
 async def handle_afk_incomming(message: Message) -> None:
@@ -131,10 +132,17 @@ async def handle_afk_incomming(message: Message) -> None:
                     f"I'm **AFK** right now, leave me alone.\nReason: {STATUS}\n"
                     f"Last Seen: `{afk_time}` ago. [\u200c]({match.group(0)})"
                 )
+                buttons = [
+                    [
+                        InlineKeyboardButton(text="🔧  CONTACT", url=="https://t.me/NoteZV"),
+                        InlineKeyboardButton(text="⚡  REPO", url=Config.UPSTREAM_REPO),
+                    ]
+                ]
+        return InlineKeyboardMarkup(buttons)
                 await userge.bot.edit_inline_text(
-                    callback_query.inline_message_id,
-                    "[\u200c](https://telegra.ph/file/c3cdea0642e1723f3304c.jpg)**FLAME GAPPS**",
-                    reply_markup=InlineKeyboardMarkup(afk_buttons()),
+                callback_query.inline_message_id,
+                "[\u200c](https://telegra.ph/file/c3cdea0642e1723f3304c.jpg)**FLAME GAPPS**",
+                reply_markup=InlineKeyboardMarkup(buttons)
                 )
             else:
                 out_str = (
