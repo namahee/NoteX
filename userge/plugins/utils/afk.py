a06f03103bf2b15257f7c098a4db66edece6f03c
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-21 16:31:34 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -5,9 +5,6 @@ import time
 from random import randint
 from re import compile as comp_regex
 
-from pyrogram.errors import BadRequest, FloodWait
-from pyrogram.types import InlineKeyboardButton
-
 from userge import Config, Message, filters, get_collection, userge
 from userge.utils import time_formatter
 
@@ -15,6 +12,9 @@ _TELE_REGEX = comp_regex(
     r"http[s]?://(telegra\.ph/file|t\.me)/(\w+)(?:\.|/)(gif|jpg|png|jpeg|mp4|[0-9]+)(?:/([0-9]+))?"
 )
 TL = comp_regex(r"[<].*[>]")
+
+from pyrogram.errors import BadRequest, FloodWait
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 
 CHANNEL = userge.getCLogger(__name__)
@@ -135,7 +135,7 @@ async def handle_afk_incomming(message: Message) -> None:
                 )
                 if allow:
                     try:
-                        await c_q.edit_message_text(
+                        await CallbackQuery.edit_message_text(
                             reply_markup=buttons,
                         )
                     except FloodWait as e:
