fbd5fc3a7361d5da755baf5f4e625cd56ae2f4fb
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-21 15:01:54 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -140,7 +140,6 @@ async def handle_afk_incomming(message: Message) -> None:
                 ]
                 await userge.bot.edit_inline_text(
                 callback_query.inline_message_id,
-                "[\u200c](https://telegra.ph/file/c3cdea0642e1723f3304c.jpg)**FLAME GAPPS**",
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             else:
