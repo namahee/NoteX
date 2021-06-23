3eab07fd91d90645b2ab5eafbb6db2eea4f2f11b
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-06-23 21:50:31 +0000

    auto: black code format

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -5,12 +5,10 @@ import time
 from random import randint
 from re import compile as comp_regex
 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 from userge import Config, Message, filters, get_collection, userge
 from userge.utils import time_formatter
-
-
-from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
-
 
 _TELE_REGEX = comp_regex(
     r"http[s]?://(telegra\.ph/file|t\.me)/(\w+)(?:\.|/)(gif|jpg|png|jpeg|mp4|[0-9]+)(?:/([0-9]+))?"
@@ -49,8 +47,6 @@ async def _init() -> None:
     },
     allow_channels=False,
 )
-
-
 # async def send_afk_buttons(IKM: InlineKeyboardMarkup, IKB: InlineKeyboardButton):
 #    buttons = IKM(
 #        [
@@ -66,8 +62,6 @@ async def _init() -> None:
 #            ]
 #        ]
 #    )
-
-
 async def active_afk(message: Message) -> None:
     """turn on or off afk mode"""
     global REASON, IS_AFK, TIME  # pylint: disable=global-statement
@@ -139,10 +133,12 @@ async def handle_afk_incomming(message: Message) -> None:
                     f"I'm **AFK** right now, leave me alone.\nReason: {STATUS}\n"
                     f"Last Seen: `{afk_time}` ago. [\u200c]({match.group(0)})"
                 )
-                reply_markup=InlineKeyboardMarkup(
+                reply_markup = InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton(text="CONTACT", url="https://t.me/NoteZV"),
+                            InlineKeyboardButton(
+                                text="CONTACT", url="https://t.me/NoteZV"
+                            ),
                             InlineKeyboardButton(text="REPO", url=Config.UPSTREAM_REPO),
                         ]
                     ]
