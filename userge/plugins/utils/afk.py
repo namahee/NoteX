11a6ad33a385e397bb0a968ece106342e980a6c9
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-06-24 17:33:46 +0000

    auto: black code format

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -107,10 +107,10 @@ async def handle_afk_incomming(message: Message) -> None:
     user_dict = await message.client.get_user_dict(user_id)
     afk_time = time_formatter(round(time.time() - TIME))
     coro_list = []
-    
+
     client = message.client
     chat_id = message.chat.id
-    
+
     contact_url = "https://t.me/NoteZV"
     buttons = InlineKeyboardMarkup(
         [
@@ -130,7 +130,7 @@ async def handle_afk_incomming(message: Message) -> None:
                     f"⚡️ **Auto Reply** ⒶⒻⓀ \n🕑 **Last Seen:** {afk_time} ago\n"
                     f"▫️ **Status**: {STATUS}"
                 )
-                
+
                 if match.group(3) == "gif" or "mp4":
                     coro_list.append(
                         client.send_animation(
@@ -155,9 +155,7 @@ async def handle_afk_incomming(message: Message) -> None:
                     f"⚡️ **Auto Reply** ⒶⒻⓀ \n🕑 **Last Seen:** {afk_time} ago\n"
                     f"▫️ **Status**: {REASON}"
                 )
-                coro_list.append(
-                    message.reply(out_str)
-                )
+                coro_list.append(message.reply(out_str))
         if chat.type == "private":
             USERS[user_id][0] += 1
         else:
@@ -196,9 +194,7 @@ async def handle_afk_incomming(message: Message) -> None:
                 f"⚡️ **Auto Reply** ⒶⒻⓀ \n🕑 **Last Seen:** {afk_time} ago\n"
                 f"▫️ **Status**: {REASON}"
             )
-            coro_list.append(
-                message.reply(out_str)
-            )
+            coro_list.append(message.reply(out_str))
         if chat.type == "private":
             USERS[user_id] = [1, 0, user_dict["mention"]]
         else:
@@ -233,6 +229,7 @@ async def handle_afk_incomming(message: Message) -> None:
     )
     await asyncio.gather(*coro_list)
 
+
 class _afk_:
     async def check_media_link(media_link: str):
         match_ = _TELE_REGEX.search(r[1])
@@ -254,7 +251,7 @@ class _afk_:
                 message_id = match.group(3)
             link = [chat_id, int(message_id)]
         return link_type, link
-    
+
     def afk_buttons() -> InlineKeyboardMarkup:
         buttons = [
             [
@@ -262,8 +259,8 @@ class _afk_:
             ]
         ]
         return InlineKeyboardMarkup(buttons)
-    
-    
+
+
 @userge.on_filters(IS_AFK_FILTER & filters.outgoing, group=-1, allow_via_bot=False)
 async def handle_afk_outgoing(message: Message) -> None:
     """handle outgoing messages when you afk"""
