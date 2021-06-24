777cff3b6e6e261698e4db078aa696b1a40da54e
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-23 21:49:07 -0300

    update quote.py

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -2,13 +2,13 @@
 # code-rgb
 
 from pyrogram.errors import YouBlockedUser
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from userge import Config, Message, userge
+from userge import Message, userge, Config
 from userge.utils.exceptions import StopConversation
 
-CHANNEL = userge.getCLogger(__name__)
 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+CHANNEL = userge.getCLogger(__name__)
 
 @userge.on_cmd(
     "q",
@@ -93,6 +93,9 @@ async def quotecmd(message: Message):
         )
 
 
+
+
+
 @userge.on_cmd(
     "git",
     about={"header": "test"},
@@ -100,7 +103,7 @@ async def quotecmd(message: Message):
 async def git_(message: Message):
     msg = "IAAEEEEEEE"
     teste = []
-
+    
     buttons = InlineKeyboardMarkup(
         [
             [
@@ -108,7 +111,7 @@ async def git_(message: Message):
             ]
         ]
     )
-
+    
     buttons2 = InlineKeyboardMarkup(
         [
             [
@@ -116,7 +119,7 @@ async def git_(message: Message):
             ]
         ]
     )
-
+    
     buttons3 = InlineKeyboardMarkup(
         [
             [
@@ -124,14 +127,12 @@ async def git_(message: Message):
             ]
         ]
     )
+    
+    await message.edit(msg)
+        
 
-    if "mr" in message.flags:
-        CHANNEL.log("Executando .git mr ...")
-        await message.edit(msg)
-    if "mg" in message.flags:
-        await message.edit(msg, reply_markup=buttons2)
-    if "g" in message.flags:
-        teste.append(message.reply(msg, reply_markup=buttons3))
+
+
 
 
 @userge.on_cmd(
