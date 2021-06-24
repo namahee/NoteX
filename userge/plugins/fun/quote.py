e7a0fcac620941bf5490d932007454929c1458ab
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-06-24 01:26:12 +0000

    auto: black code format

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -1,15 +1,14 @@
 # Rewrote with experimental bleck magik
 # code-rgb
 
-from pyrogram.errors import YouBlockedUser
+from pyrogram.errors import FloodWait, YouBlockedUser
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from userge import Message, userge, Config
+from userge import Config, Message, userge
 from userge.utils.exceptions import StopConversation
 
-
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 CHANNEL = userge.getCLogger(__name__)
-from pyrogram.errors import BadRequest, FloodWait, UserIsBlocked
+
 
 @userge.on_cmd(
     "q",
@@ -94,21 +93,19 @@ async def quotecmd(message: Message):
         )
 
 
-async def send_buttons(message: Message, text: str, markup: InlineKeyboardMarkup) -> None:
-    await message.reply(
-        text, disable_web_page_preview=True, reply_markup=markup)
-    
+async def send_buttons(
+    message: Message, text: str, markup: InlineKeyboardMarkup
+) -> None:
+    await message.reply(text, disable_web_page_preview=True, reply_markup=markup)
 
 
 @userge.on_cmd(
     "git",
-    about={
-        "header": "test"},
+    about={"header": "test"},
 )
 async def git_(message: Message):
     msg = "IAAEEEEEEE"
-    teste = []
-    
+
     buttons = InlineKeyboardMarkup(
         [
             [
@@ -116,7 +113,7 @@ async def git_(message: Message):
             ]
         ]
     )
-    
+
     buttons2 = InlineKeyboardMarkup(
         [
             [
@@ -124,27 +121,19 @@ async def git_(message: Message):
             ]
         ]
     )
-    
+
     buttons3 = [
         [
             InlineKeyboardButton(text="GITHUB", url="https://github.com/"),
         ]
     ]
-    
+
     try:
         await send_buttons(message, msg, InlineKeyboardMarkup(buttons3))
     except FloodWait as e:
         await asyncio.sleep(e.x + 10)
     except Exception as bpm_e:
-        await CHANNEL.log(
-                f"**ERRO**: {str(bpm_e)}\n\nOps, aconteceu algo estranho.."
-        )
-    
-    
-        
-
-
-
+        await CHANNEL.log(f"**ERRO**: {str(bpm_e)}\n\nOps, aconteceu algo estranho..")
 
 
 @userge.on_cmd(
