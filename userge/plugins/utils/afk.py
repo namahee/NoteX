6f0e928393cc090f999e49d398f9b3fe8f7865ab
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-24 13:56:11 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -99,26 +99,28 @@ async def active_afk(message: Message) -> None:
     ),
     allow_via_bot=False,
 )
+
 # async def check_media_link(media_link: str):
-# matchh = _TELE_REGEX.search(media_link)
-# if not matchh:
-# return None, None
-# if matchh.group(1) == "i.imgur.com":
-# link = matchh.group(0)
-# link_type = "url_gif" if matchh.group(3) == "gif" else "url_image"
-# elif matchh.group(1) == "telegra.ph/file":
-# link = matchh.group(0)
-# link_type = "url_image"
-# else:
-# link_type = "tg_media"
-# if matchh.group(2) == "c":
-# chat_id = int("-100" + str(matchh.group(3)))
-# message_id = matchh.group(4)
-# else:
-# chat_id = matchh.group(2)
-# message_id = matchh.group(3)
-# link = [chat_id, int(message_id)]
-# return link_type, link
+    # matchh = _TELE_REGEX.search(media_link)
+    # if not matchh:
+        # return None, None
+    # if matchh.group(1) == "i.imgur.com":
+        # link = matchh.group(0)
+        # link_type = "url_gif" if matchh.group(3) == "gif" else "url_image"
+    # elif matchh.group(1) == "telegra.ph/file":
+        # link = matchh.group(0)
+        # link_type = "url_image"
+    # else:
+        # link_type = "tg_media"
+        # if matchh.group(2) == "c":
+            # chat_id = int("-100" + str(matchh.group(3)))
+            # message_id = matchh.group(4)
+        # else:
+            # chat_id = matchh.group(2)
+            # message_id = matchh.group(3)
+        # link = [chat_id, int(message_id)]
+    # return link_type, link
+
 async def handle_afk_incomming(message: Message) -> None:
     """handle incomming messages when you afk"""
     if not message.from_user:
@@ -128,10 +130,10 @@ async def handle_afk_incomming(message: Message) -> None:
     user_dict = await message.client.get_user_dict(user_id)
     afk_time = time_formatter(round(time.time() - TIME))
     coro_list = []
-
+    
     client = message.client
     chat_id = message.chat.id
-
+    
     contact_url = "https://t.me/NoteZV"
     buttons = InlineKeyboardMarkup(
         [
@@ -152,15 +154,15 @@ async def handle_afk_incomming(message: Message) -> None:
                     f"▫️ **Status**: {STATUS}"
                 )
                 # type_, media_ = check_media_link(match.group(0))
-                # if type_ == "jpg" or "png" or "jpeg":
-                # coro_list.append(
-                # client.send_photo(
-                # chat_id,
-                # photo=match.group(0),
-                # caption=out_str,
-                # reply_markup=buttons,
-                # )
-                # )
+               # if type_ == "jpg" or "png" or "jpeg":
+                    # coro_list.append(
+                        # client.send_photo(
+                            # chat_id,
+                            # photo=match.group(0),
+                            # caption=out_str,
+                            # reply_markup=buttons,
+                        # )
+                    # )
                 if match.group(3) == "gif" or "mp4":
                     coro_list.append(
                         client.send_animation(
@@ -185,7 +187,9 @@ async def handle_afk_incomming(message: Message) -> None:
                     f"⚡️ **Auto Reply** ⒶⒻⓀ \n🕑 **Last Seen:** {afk_time} ago\n"
                     f"▫️ **Status**: {REASON}"
                 )
-                coro_list.append(message.reply(out_str))
+                coro_list.append(
+                    message.reply(out_str)
+                )
         if chat.type == "private":
             USERS[user_id][0] += 1
         else:
@@ -199,7 +203,7 @@ async def handle_afk_incomming(message: Message) -> None:
                 f"⚡️ **Auto Reply** ⒶⒻⓀ \n🕑 **Last Seen:** {afk_time} ago\n"
                 f"▫️ **Status**: {STATUS}"
             )
-            if match.group(3) == "jpg" or "png" or "jpeg":
+            if not match.group(3) == "gif" or "mp4":
                 coro_list.append(
                     client.send_photo(
                         chat_id,
@@ -209,7 +213,7 @@ async def handle_afk_incomming(message: Message) -> None:
                     )
                 )
             else:
-                if match.group(3) == "gif" or "mp4":
+                if not match.group(3) == "jpg" or "png" or "jpeg":
                     coro_list.append(
                         client.send_animation(
                             chat_id,
@@ -219,20 +223,22 @@ async def handle_afk_incomming(message: Message) -> None:
                         )
                     )
             # else:
-            # coro_list.append(
-            # client.send_photo(
-            # chat_id,
-            # photo=match.group(0),
-            # caption=out_str,
-            # reply_markup=buttons,
-            # )
-            # )
+                # coro_list.append(
+                    # client.send_photo(
+                        # chat_id,
+                        # photo=match.group(0),
+                        # caption=out_str,
+                        # reply_markup=buttons,
+                    # )
+                # )
         else:
             out_str = (
                 f"⚡️ **Auto Reply** ⒶⒻⓀ \n🕑 **Last Seen:** {afk_time} ago\n"
                 f"▫️ **Status**: {REASON}"
             )
-            coro_list.append(message.reply(out_str))
+            coro_list.append(
+                message.reply(out_str)
+            )
         if chat.type == "private":
             USERS[user_id] = [1, 0, user_dict["mention"]]
         else:
@@ -267,7 +273,6 @@ async def handle_afk_incomming(message: Message) -> None:
     )
     await asyncio.gather(*coro_list)
 
-
 class _afk_:
     def afk_buttons() -> InlineKeyboardMarkup:
         buttons = [
@@ -276,8 +281,8 @@ class _afk_:
             ]
         ]
         return InlineKeyboardMarkup(buttons)
-
-
+    
+    
 @userge.on_filters(IS_AFK_FILTER & filters.outgoing, group=-1, allow_via_bot=False)
 async def handle_afk_outgoing(message: Message) -> None:
     """handle outgoing messages when you afk"""
