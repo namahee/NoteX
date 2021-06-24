120b43c03fa7677bedbef8e3d207eebb500abe39
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-06-24 03:20:54 +0000

    auto: black code format

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -2,14 +2,13 @@
 # code-rgb
 
 from pyrogram.errors import YouBlockedUser
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from userge import Message, userge, Config
+from userge import Config, Message, userge
 from userge.utils.exceptions import StopConversation
 
-
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 CHANNEL = userge.getCLogger(__name__)
-from pyrogram.errors import BadRequest, FloodWait
+
 
 @userge.on_cmd(
     "q",
@@ -94,10 +93,12 @@ async def quotecmd(message: Message):
         )
 
 
-async def send_buttons(message: Message, text: str, markup: InlineKeyboardMarkup) -> None:
-    await message.reply(
-        text, disable_web_page_preview=True, reply_markup=markup)
-    
+async def send_buttons(
+    message: Message, text: str, markup: InlineKeyboardMarkup
+) -> None:
+    await message.reply(text, disable_web_page_preview=True, reply_markup=markup)
+
+
 def _info() -> str:
     _info_ = f"""
 <a href="https://telegram.dog/x_xtests"><b>NoteX</a> is Up and Running.</b>
@@ -109,16 +110,14 @@ o</code>
 
 <b>botalive</b>    <code>|</code>    🕔  <b>uptime</b>
 """
-        return _info_
+    return _info_
+
 
 @userge.on_cmd(
     "git",
-    about={
-        "header": "test"},
+    about={"header": "test"},
 )
 async def git_(message: Message):
-    msg = "IAAEEEEEEE"
-    teste = []
     cap = _info()
     chat_id = message.chat.id
     client = message.client
@@ -128,7 +127,7 @@ async def git_(message: Message):
         "    <code>|</code>    "
         "👥  <a href='https://t.me/useless_x'><b>SUPPORT</b></a>"
     )
-        
+
     buttons = InlineKeyboardMarkup(
         [
             [
@@ -136,7 +135,7 @@ async def git_(message: Message):
             ]
         ]
     )
-    
+
     buttons2 = InlineKeyboardMarkup(
         [
             [
@@ -144,36 +143,30 @@ async def git_(message: Message):
             ]
         ]
     )
-    
+
     buttons3 = [
         [
             InlineKeyboardButton(text="GITHUB", url="https://github.com/"),
         ]
     ]
-    
+
     await client.send_photo(
         chat_id,
         photo=foto,
         caption=cap,
         reply_markup=buttons,
     )
-    
+
     # await message.reply(msg, reply_markup=buttons2)
-    
+
     # try:
-        # await send_buttons(message, msg, InlineKeyboardMarkup(buttons3))
+    # await send_buttons(message, msg, InlineKeyboardMarkup(buttons3))
     # except FloodWait as e:
-        # await asyncio.sleep(e.x + 10)
+    # await asyncio.sleep(e.x + 10)
     # except Exception as bpm_e:
-        # await CHANNEL.log(
-            # f"**ERRO**: {str(bpm_e)}\n\nOps, aconteceu algo estranho..."
-        # )
-    
-    
-        
-
-
-
+    # await CHANNEL.log(
+    # f"**ERRO**: {str(bpm_e)}\n\nOps, aconteceu algo estranho..."
+    # )
 
 
 @userge.on_cmd(
