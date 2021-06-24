da37fa2089ea28160530154a32ec39ae5f8ed2e7
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-24 01:34:23 -0300

    update afk.py

--- userge/plugins/utils/afk.py
+++ userge/plugins/utils/afk.py
@@ -142,7 +142,9 @@ async def handle_afk_incomming(message: Message) -> None:
                     f"I'm **AFK** right now, leave me alone.\nReason: {REASON}\n"
                     f"Last Seen: `{afk_time}` ago"
                 )
-                coro_list.append(message.reply(out_str))
+                coro_list.append(
+                    message.reply(out_str)
+                )
         if chat.type == "private":
             USERS[user_id][0] += 1
         else:
@@ -169,7 +171,9 @@ async def handle_afk_incomming(message: Message) -> None:
                 f"I'm **AFK** right now, leave me alone.\nReason: {REASON}\n"
                 f"Last Seen: `{afk_time}` ago"
             )
-            coro_list.append(message.reply(out_str))
+            coro_list.append(
+                message.reply(out_str)
+            )
         if chat.type == "private":
             USERS[user_id] = [1, 0, user_dict["mention"]]
         else:
