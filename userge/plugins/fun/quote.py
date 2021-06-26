c755f488eb99097156c125bdb5fd56022e664b78
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-26 19:53:08 -0300

    update quote.py

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -2,16 +2,40 @@
 # code-rgb
 
 import asyncio
+
+from pyrogram.errors import YouBlockedUser
+
+from userge import Message, userge
+from userge.utils.exceptions import StopConversation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import random
+from datetime import datetime
 from re import compile as comp_regex
 
-from pyrogram.errors import BadRequest, Forbidden, YouBlockedUser
+from pyrogram import filters
+from pyrogram.errors import BadRequest, FloodWait, Forbidden, MediaEmpty
 from pyrogram.file_id import PHOTO_TYPES, FileId
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
-from userge import Config, Message, userge
+from userge import Config, Message, get_version, userge, versions
+from userge.core.ext import RawClient
 from userge.utils import get_file_id, rand_array
-from userge.utils.exceptions import StopConversation
 
 _ALIVE_REGEX = comp_regex(
     r"http[s]?://(i\.imgur\.com|telegra\.ph/file|t\.me)/(\w+)(?:\.|/)(gif|jpg|png|jpeg|[0-9]+)(?:/([0-9]+))?"
@@ -41,7 +65,6 @@ async def _init() -> None:
             except Exception as b_rr:
                 LOGGER.debug(b_rr)
 
-
 @userge.on_cmd("t", about={"header": "Just For Fun"}, allow_channels=False)
 async def alive_inline(message: Message):
     try:
@@ -56,7 +79,6 @@ async def alive_inline(message: Message):
             await send_alive_message(message)
     except Exception as e_all:
         await message.err(str(e_all), del_in=10, log=__name__)
-
 
 async def send_inline_alive(message: Message) -> None:
     _bot = await userge.bot.get_me()
@@ -80,7 +102,6 @@ async def send_inline_alive(message: Message) -> None:
     await asyncio.sleep(60)
     await userge.delete_messages(message.chat.id, i_res_id)
 
-
 TOI = (
     "https://telegra.ph/file/f5db2ec096a584052feb0.jpg",
     "https://telegra.ph/file/712d78c5cd60f369be907.gif",
@@ -97,9 +118,14 @@ async def send_alive_message(message: Message) -> None:
     cap = Bot_t.t_info()
     url_ = random.choice(TOI).strip()
     if client.is_bot:
-        Bot_t.t_buttons()
+        reply_markup = Bot_t.t_buttons()
+        file_id = _BOT_CACHED_MEDIA
     else:
-        caption += "IIOOOOOOOOO"
+        reply_markup = None
+        file_id = _USER_CACHED_MEDIA
+        caption += (
+            "IIOOOOOOOOO"
+        )
     if not url:
         await client.send_photo(
             chat_id,
@@ -117,7 +143,7 @@ async def send_alive_message(message: Message) -> None:
             disable_web_page_preview=True,
         )
     else:
-        type_, media_ = await Bot_Alive.check_media_link(Config.ALIVE_MEDIA)
+        type_, media_ = await Bot_Alive.check_media_link(url_)
         if type_ == "url_gif":
             await client.send_animation(
                 chat_id,
@@ -159,7 +185,9 @@ class Bot_t:
 
     @staticmethod
     def t_info() -> str:
-        t_info_ = "OOIIIIIIII"
+        t_info_ = (
+            "OOIIIIIIII"
+        )
         return t_info_
 
     @staticmethod
@@ -189,6 +217,31 @@ class Bot_t:
     @staticmethod
     def is_photo(file_id: str) -> bool:
         return bool(FileId.decode(file_id).file_type in PHOTO_TYPES)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @userge.on_cmd(
@@ -328,3 +381,4 @@ async def check_and_send(message: Message, *args, **kwargs):
         await asyncio.gather(message.delete(), replied.reply(*args, **kwargs))
     else:
         await message.edit(*args, **kwargs)
+        
