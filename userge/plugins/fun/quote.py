5fd34eadc617c640fdafbede44b5117958e59aa6
samuca78 <62891774+samuca78@users.noreply.github.com>
2021-06-26 22:10:36 +0000

    auto: @samuca78

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -2,40 +2,16 @@
 # code-rgb
 
 import asyncio
-
-from pyrogram.errors import YouBlockedUser
-
-from userge import Message, userge
-from userge.utils.exceptions import StopConversation
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import random
-from datetime import datetime
 from re import compile as comp_regex
 
-from pyrogram import filters
-from pyrogram.errors import BadRequest, FloodWait, Forbidden, MediaEmpty
+from pyrogram.errors import YouBlockedUser
 from pyrogram.file_id import PHOTO_TYPES, FileId
-from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from userge import Config, Message, get_version, userge, versions
-from userge.core.ext import RawClient
+from userge import Config, Message, userge
 from userge.utils import get_file_id, rand_array
+from userge.utils.exceptions import StopConversation
 
 _ALIVE_REGEX = comp_regex(
     r"http[s]?://(i\.imgur\.com|telegra\.ph/file|t\.me)/(\w+)(?:\.|/)(gif|jpg|png|jpeg|[0-9]+)(?:/([0-9]+))?"
@@ -65,6 +41,7 @@ async def _init() -> None:
             except Exception as b_rr:
                 LOGGER.debug(b_rr)
 
+
 @userge.on_cmd("t", about={"header": "Just For Fun"}, allow_channels=False)
 async def alive_inline(message: Message):
     try:
@@ -90,7 +67,7 @@ async def send_alive_message(message: Message) -> None:
     chat_id = message.chat.id
     client = message.client
     cap = Bot_t.t_info()
-    
+
     url_ = random.choice(TOI)
     type_, media_ = await Bot_t.check_media_link(url_)
     if type_ == "url_gif":
@@ -134,9 +111,7 @@ class Bot_t:
 
     @staticmethod
     def t_info() -> str:
-        t_info_ = (
-            "OOIIIIIIII"
-        )
+        t_info_ = "OOIIIIIIII"
         return t_info_
 
     @staticmethod
@@ -166,31 +141,6 @@ class Bot_t:
     @staticmethod
     def is_photo(file_id: str) -> bool:
         return bool(FileId.decode(file_id).file_type in PHOTO_TYPES)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 @userge.on_cmd(
@@ -330,4 +280,3 @@ async def check_and_send(message: Message, *args, **kwargs):
         await asyncio.gather(message.delete(), replied.reply(*args, **kwargs))
     else:
         await message.edit(*args, **kwargs)
-        
