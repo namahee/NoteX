11442f7a469a5e75d9aab2720042afb59ee1e6ee
samuca78 <62891774+samuca78@users.noreply.github.com>
2021-06-27 00:55:14 +0000

    auto: @samuca78

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -4,11 +4,11 @@
 import asyncio
 
 from pyrogram.errors import YouBlockedUser
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from userge import Message, userge
 from userge.utils.exceptions import StopConversation
 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @userge.on_cmd(
     "q",
@@ -147,8 +147,8 @@ async def check_and_send(message: Message, *args, **kwargs):
         await asyncio.gather(message.delete(), replied.reply(*args, **kwargs))
     else:
         await message.edit(*args, **kwargs)
-        
-        
+
+
 @userge.on_cmd(
     "ttt",
     about={
@@ -163,8 +163,11 @@ async def send_inline(message: Message):
     )
     await message.delete()
 
+
 async def tt(message: Message):
     await message.edit_text("ooi", reply_markup=idk.buttons())
+
+
 class idk:
     def buttons() -> InlineKeyboardMarkup:
         buttons = [
@@ -172,4 +175,3 @@ class idk:
                 InlineKeyboardButton(text="oi", url="https://google.com"),
             ]
         ]
-        
