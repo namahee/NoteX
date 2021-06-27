40ea9b9d93635244640df80847b804cd76d4db52
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-27 02:19:04 -0300

    update help.py

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -27,6 +27,8 @@ from userge.utils import get_file_id, get_response
 from userge.utils import parse_buttons as pb
 from userge.utils import rand_key
 
+from .custom.afk import _afk_
+from .custom import afk
 from .bot.alive import Bot_Alive
 from .bot.gogo import Anime
 from .bot.utube_inline import (
@@ -36,7 +38,6 @@ from .bot.utube_inline import (
     result_formatter,
     ytsearch_data,
 )
-from .custom.afk import _afk_
 from .fun.stylish import Styled, font_gen
 from .misc.redditdl import reddit_thumb_link
 from .utils.notes import get_inote
@@ -696,9 +697,10 @@ if userge.has_bot:
                                         reply_markup=buttons,
                                     )
                                 )
-
+                                
+                                
             if string == "afk":
-                alive_info = _afk_.out_str()
+                alive_info = Bot_Alive.alive_info()
                 buttons = Bot_Alive.alive_buttons()
                 if not Config.ALIVE_MEDIA:
                     results.append(
@@ -740,7 +742,7 @@ if userge.has_bot:
                                     reply_markup=buttons,
                                 )
                             )
-
+                                
             if string == "geass":
                 results.append(
                     InlineQueryResultAnimation(
@@ -789,10 +791,10 @@ if userge.has_bot:
             if string == "t":
                 buttons = [
                     [
+                        InlineKeyboardButton("My Repo", url="https://github.com/samuca78/NoteX"),
                         InlineKeyboardButton(
-                            "My Repo", url="https://github.com/samuca78/NoteX"
+                            "Github", url="https://github.com"
                         ),
-                        InlineKeyboardButton("Github", url="https://github.com"),
                     ],
                     [InlineKeyboardButton("My Git", url="https://github.com/samuca78")],
                 ]
