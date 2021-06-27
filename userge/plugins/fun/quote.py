28fe19d28615abab6d17e1316353e242c916d06b
samuca78 <62891774+samuca78@users.noreply.github.com>
2021-06-27 01:14:38 +0000

    auto: @samuca78

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -4,11 +4,11 @@
 import asyncio
 
 from pyrogram.errors import YouBlockedUser
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from userge import Message, userge
 from userge.utils.exceptions import StopConversation
 
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @userge.on_cmd(
     "q",
@@ -147,8 +147,8 @@ async def check_and_send(message: Message, *args, **kwargs):
         await asyncio.gather(message.delete(), replied.reply(*args, **kwargs))
     else:
         await message.edit(*args, **kwargs)
-        
-        
+
+
 @userge.on_cmd(
     "t",
     about={
@@ -156,17 +156,17 @@ async def check_and_send(message: Message, *args, **kwargs):
     },
 )
 # async def send_inline(message: Message):
-   # bot = await userge.bot.get_me()
-    # x = await userge.get_inline_bot_results(bot.username, "quote")
-    # await userge.send_inline_bot_result(
-        # chat_id=message.chat.id, query_id=x.query_id, result_id=x.results[0].id
-    # )
-    # await message.delete()
-
+# bot = await userge.bot.get_me()
+# x = await userge.get_inline_bot_results(bot.username, "quote")
+# await userge.send_inline_bot_result(
+# chat_id=message.chat.id, query_id=x.query_id, result_id=x.results[0].id
+# )
+# await message.delete()
 async def tt(message: Message):
-    botname = (await userge.bot.get_me()).username
+    (await userge.bot.get_me()).username
     await message.edit_text("ooi", reply_markup=idk.buttons())
-    
+
+
 class idk:
     def buttons() -> InlineKeyboardMarkup:
         buttons = [
@@ -174,4 +174,3 @@ class idk:
                 InlineKeyboardButton(text="oi", url="https://google.com"),
             ]
         ]
-        
