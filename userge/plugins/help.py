79b4e0b8bb6dbdf150fe439ee1f04a51db0ab35c
samuca78 <62891774+samuca78@users.noreply.github.com>
2021-06-27 16:57:10 +0000

    auto: @samuca78

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -36,7 +36,6 @@ from .bot.utube_inline import (
     result_formatter,
     ytsearch_data,
 )
-from .custom.afk import _afk_
 from .fun.stylish import Styled, font_gen
 from .misc.redditdl import reddit_thumb_link
 from .utils.notes import get_inote
@@ -696,7 +695,7 @@ if userge.has_bot:
                                         reply_markup=buttons,
                                     )
                                 )
-                                
+
             if string == "gesso":
                 results.append(
                     InlineQueryResultPhoto(
@@ -705,8 +704,6 @@ if userge.has_bot:
                         reply_markup=Bot_Alive.alive_buttons(),
                     )
                 )
-
-            
 
             if string == "geass":
                 results.append(
