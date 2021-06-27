25994032284927e65877079382fc97054c9cbf43
samuca78 <62891774+samuca78@users.noreply.github.com>
2021-06-27 05:35:14 +0000

    auto: @samuca78

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -27,8 +27,6 @@ from userge.utils import get_file_id, get_response
 from userge.utils import parse_buttons as pb
 from userge.utils import rand_key
 
-from .custom.afk import _afk_
-from .custom import afk
 from .bot.alive import Bot_Alive
 from .bot.gogo import Anime
 from .bot.utube_inline import (
@@ -38,6 +36,7 @@ from .bot.utube_inline import (
     result_formatter,
     ytsearch_data,
 )
+from .custom.afk import _afk_
 from .fun.stylish import Styled, font_gen
 from .misc.redditdl import reddit_thumb_link
 from .utils.notes import get_inote
@@ -697,14 +696,11 @@ if userge.has_bot:
                                         reply_markup=buttons,
                                     )
                                 )
-                                
-                                
+
             if string == "afk":
                 alive_info = _afk_.out_str()
                 buttons = _afk_.afk_buttons()
-                _media_type, _media_url = await _afk_.check_media_link(
-                    match.group(0)
-                )
+                _media_type, _media_url = await _afk_.check_media_link(match.group(0))
                 if _media_type == "url_gif":
                     results.append(
                         InlineQueryResultAnimation(
@@ -722,46 +718,46 @@ if userge.has_bot:
                         )
                     )
                 # if not Config.ALIVE_MEDIA:
-                    # results.append(
-                        # InlineQueryResultPhoto(
-                            # photo_url=Bot_Alive.alive_default_imgs(),
-                            # caption=alive_info,
-                            # reply_markup=buttons,
-                        # )
-                    # )
+                # results.append(
+                # InlineQueryResultPhoto(
+                # photo_url=Bot_Alive.alive_default_imgs(),
+                # caption=alive_info,
+                # reply_markup=buttons,
+                # )
+                # )
                 # else:
-                    # if Config.ALIVE_MEDIA.lower().strip() == "false":
-                        # results.append(
-                            # InlineQueryResultArticle(
-                                # title="USERGE-X",
-                                # input_message_content=InputTextMessageContent(
-                                    # alive_info, disable_web_page_preview=True
-                                # ),
-                                # description="ALIVE",
-                                # reply_markup=buttons,
-                            # )
-                        # )
-                    # else:
-                        # _media_type, _media_url = await _afk_.check_media_link(
-                            # match.group(0)
-                        # )
-                        # if _media_type == "url_gif":
-                            # results.append(
-                                # InlineQueryResultAnimation(
-                                    # animation_url=_media_url,
-                                    # caption=alive_info,
-                                    # reply_markup=buttons,
-                                # )
-                            # )
-                        # elif _media_type == "url_image":
-                            # results.append(
-                                # InlineQueryResultPhoto(
-                                    # photo_url=_media_url,
-                                    # caption=alive_info,
-                                    # reply_markup=buttons,
-                                # )
-                            # )
-                                
+                # if Config.ALIVE_MEDIA.lower().strip() == "false":
+                # results.append(
+                # InlineQueryResultArticle(
+                # title="USERGE-X",
+                # input_message_content=InputTextMessageContent(
+                # alive_info, disable_web_page_preview=True
+                # ),
+                # description="ALIVE",
+                # reply_markup=buttons,
+                # )
+                # )
+                # else:
+                # _media_type, _media_url = await _afk_.check_media_link(
+                # match.group(0)
+                # )
+                # if _media_type == "url_gif":
+                # results.append(
+                # InlineQueryResultAnimation(
+                # animation_url=_media_url,
+                # caption=alive_info,
+                # reply_markup=buttons,
+                # )
+                # )
+                # elif _media_type == "url_image":
+                # results.append(
+                # InlineQueryResultPhoto(
+                # photo_url=_media_url,
+                # caption=alive_info,
+                # reply_markup=buttons,
+                # )
+                # )
+
             if string == "geass":
                 results.append(
                     InlineQueryResultAnimation(
@@ -810,10 +806,10 @@ if userge.has_bot:
             if string == "t":
                 buttons = [
                     [
-                        InlineKeyboardButton("My Repo", url="https://github.com/samuca78/NoteX"),
                         InlineKeyboardButton(
-                            "Github", url="https://github.com"
+                            "My Repo", url="https://github.com/samuca78/NoteX"
                         ),
+                        InlineKeyboardButton("Github", url="https://github.com"),
                     ],
                     [InlineKeyboardButton("My Git", url="https://github.com/samuca78")],
                 ]
