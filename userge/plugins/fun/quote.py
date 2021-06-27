8703f23d54baf44537fc32fe439e645aadebfc76
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-26 21:45:32 -0300

    update quote.py

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -4,11 +4,11 @@
 import asyncio
 
 from pyrogram.errors import YouBlockedUser
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from userge import Message, userge
 from userge.utils.exceptions import StopConversation
 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @userge.on_cmd(
     "q",
@@ -147,8 +147,8 @@ async def check_and_send(message: Message, *args, **kwargs):
         await asyncio.gather(message.delete(), replied.reply(*args, **kwargs))
     else:
         await message.edit(*args, **kwargs)
-
-
+        
+        
 @userge.on_cmd(
     "tt",
     about={
@@ -157,7 +157,14 @@ async def check_and_send(message: Message, *args, **kwargs):
 )
 async def tt(message: Message):
     await message.edit_text("ooi", reply_markup=idk.buttons())
-
+    
+async def send_inline(message: Message):
+    bot = await userge.bot.get_me()
+    x = await userge.get_inline_bot_results(bot.username, "tt")
+    await userge.send_inline_bot_result(
+        chat_id=message.chat.id, query_id=x.query_id, result_id=x.results[0].id
+    )
+    await message.delete()
 
 class idk:
     def buttons() -> InlineKeyboardMarkup:
@@ -166,3 +173,4 @@ class idk:
                 InlineKeyboardButton(text="oi", url="https://google.com"),
             ]
         ]
+        
