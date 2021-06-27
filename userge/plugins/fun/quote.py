0eb35a4c7d3b8b2bf7617417b44eb5a1521f8da7
samuca78 <62891774+samuca78@users.noreply.github.com>
2021-06-27 02:05:23 +0000

    auto: @samuca78

--- userge/plugins/fun/quote.py
+++ userge/plugins/fun/quote.py
@@ -3,13 +3,12 @@
 
 import asyncio
 
-from pyrogram.errors import YouBlockedUser
+from pyrogram.errors import BadRequest, Forbidden, YouBlockedUser
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from userge import Message, userge
 from userge.utils.exceptions import StopConversation
 
-from pyrogram.errors import BadRequest, FloodWait, Forbidden, MediaEmpty
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @userge.on_cmd(
     "q",
@@ -148,8 +147,8 @@ async def check_and_send(message: Message, *args, **kwargs):
         await asyncio.gather(message.delete(), replied.reply(*args, **kwargs))
     else:
         await message.edit(*args, **kwargs)
-        
-        
+
+
 @userge.on_cmd(
     "t",
     about={
@@ -178,17 +177,19 @@ async def send_inline_alive(message: Message) -> None:
     await asyncio.sleep(60)
     await userge.delete_messages(message.chat.id, i_res_id)
 
+
 # async def send_inline(message: Message):
-   # bot = await userge.bot.get_me()
-   # x = await userge.get_inline_bot_results(bot.username, "t")
-   # await userge.send_inline_bot_result(
-       # chat_id=message.chat.id, query_id=x.query_id, result_id=x.results[0].id
-   # )
-   # await message.delete()
+# bot = await userge.bot.get_me()
+# x = await userge.get_inline_bot_results(bot.username, "t")
+# await userge.send_inline_bot_result(
+# chat_id=message.chat.id, query_id=x.query_id, result_id=x.results[0].id
+# )
+# await message.delete()
 
 # async def tt(message: Message):
-    # await message.edit_text("ooi", reply_markup=idk.buttons())
-    
+# await message.edit_text("ooi", reply_markup=idk.buttons())
+
+
 class idk:
     def buttons() -> InlineKeyboardMarkup:
         buttons = [
@@ -196,4 +197,3 @@ class idk:
                 InlineKeyboardButton(text="oi", url="https://google.com"),
             ]
         ]
-        
