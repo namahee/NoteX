8da69bae8267f62a57dbaf1cee7dfb21a58b1aa1
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-28 00:03:39 -0300

    update afk_inline.py

--- userge/plugins/utils/afk_inline.py
+++ userge/plugins/utils/afk_inline.py
@@ -1,5 +1,8 @@
 from userge import Message, userge
+from userge.plugins.custom.afk import _TELE_REGEX, REASON
+from userge.utils import time_formatter
 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 async def send_inline_afk(message: Message):
     bot = await userge.bot.get_me()
@@ -23,3 +26,61 @@ async def _send_inline_afk(message: Message):
     await userge.send_inline_bot_result(
         chat_id=message.chat.id, query_id=_x.query_id, result_id=_x.results[0].id
     )
+
+class _afk_:
+    def out_str() -> str:
+        _afk_time = time_formatter(round(time.time() - TIME))
+        _r = REASON.split(" | ", maxsplit=1)
+        _STATUS = _r[0]
+        out_str = (
+            f"⚡️ **Auto Reply** ⒶⒻⓀ \n ╰•  **Last Check:** {_afk_time} ago\n\n"
+            f"▫️ **I'm not here because:**\n {_STATUS}"
+        )
+        return out_str
+        
+    def _out_str() -> str:
+        afk_time_ = time_formatter(round(time.time() - TIME))
+        out_str = (
+            f"⚡️ **Auto Reply** ⒶⒻⓀ \n ╰•  **Last Check:** {afk_time_} ago.\n\n"
+            f"▫️ **I'm not here because:**\n {REASON}"
+        )
+        return out_str
+    
+    def link() -> str:
+        _match_ =  _TELE_REGEX.search(REASON)
+        if _match_:
+            link = _match_.group(0)
+            return link
+    
+    async def check_media_link(media_link: str):
+        match_ = _TELE_REGEX.search(media_link.strip())
+        if not match_:
+            return None, None
+        if match_.group(1) == "i.imgur.com":
+            link = match_.group(0)
+            link_type = "url_gif" if match_.group(3) == "gif" else "url_image"
+        elif match_.group(1) == "telegra.ph/file":
+            link = match_.group(0)
+            link_type = "url_gif" if match_.group(3) == "gif" or "mp4" else "url_image"
+        else:
+            link_type = "tg_media"
+            if match_.group(2) == "c":
+                chat_id = int("-100" + str(match_.group(3)))
+                message_id = match_.group(4)
+            else:
+                chat_id = match_.group(2)
+                message_id = match_.group(3)
+            link = [chat_id, int(message_id)]
+        return link_type, link
+
+    def afk_buttons() -> InlineKeyboardMarkup:
+        buttons = [
+            [
+                InlineKeyboardButton("My Repo", url="https://github.com/samuca78/NoteX"),
+                InlineKeyboardButton("Github", url="https://github.com"),
+            ],
+            [
+                InlineKeyboardButton("My Git", url="https://github.com/samuca78"),
+            ],
+        ]
+        return InlineKeyboardMarkup(buttons)
