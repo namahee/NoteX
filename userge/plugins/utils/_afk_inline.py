59c9dda1ecdfe4f68db250d2fe7d6f8b73aac11e
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-27 23:52:18 -0300

    Delete _afk_inline.py

--- userge/plugins/utils/_afk_inline.py
+++
@@ -1,9 +0,0 @@
-from userge import Message, userge
-
-
-async def _send_inline_afk(message: Message):
-    _bot = await userge.bot.get_me()
-    _x = await userge.get_inline_bot_results(_bot.username, "_afk")
-    await userge.send_inline_bot_result(
-        chat_id=message.chat.id, query_id=_x.query_id, result_id=_x.results[0].id
-    )
