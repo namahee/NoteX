ded3464f12323ec245cdc93051076247bf78f2c1
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-28 15:09:53 -0300

    update afk_inline.py

--- userge/plugins/utils/afk_inline.py
+++ userge/plugins/utils/afk_inline.py
@@ -1,8 +1,7 @@
 from userge import Message, userge
 
-# from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-# from userge.plugins.custom.afk import _TELE_REGEX
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from userge.plugins.custom.afk import _TELE_REGEX
 
 async def send_inline_afk(message: Message):
     bot = await userge.bot.get_me()
@@ -28,38 +27,35 @@ async def _send_inline_afk(message: Message):
     )
 
 
-# class _afk:
-# async def check_media_link(media_link: str):
-# match_ = _TELE_REGEX.search(media_link.strip())
-# if not match_:
-# return None, None
-# if match_.group(1) == "i.imgur.com":
-# link = match_.group(0)
-# link_type = "url_gif" if match_.group(3) == "gif" else "url_image"
-# elif match_.group(1) == "telegra.ph/file":
-# link = match_.group(0)
-# link_type = "url_gif" if match_.group(3) == "gif" else "url_image"
-# else:
-# link_type = "tg_media"
-# if match_.group(2) == "c":
-# chat_id = int("-100" + str(match_.group(3)))
-# message_id = match_.group(4)
-# else:
-# chat_id = match_.group(2)
-# message_id = match_.group(3)
-# link = [chat_id, int(message_id)]
-# return link_type, link
+class _afk:
+    async def check_media_link(media_link: str):
+        match_ = _TELE_REGEX.search(media_link.strip())
+        if not match_:
+            return None, None
+        if match_.group(1) == "i.imgur.com":
+            link = match_.group(0)
+            link_type = "url_gif" if match_.group(3) == "gif" else "url_image"
+        elif match_.group(1) == "telegra.ph/file":
+            link = match_.group(0)
+            link_type = "url_gif" if match_.group(3) == "gif" else "url_image"
+        else:
+            link_type = "tg_media"
+            if match_.group(2) == "c":
+                chat_id = int("-100" + str(match_.group(3)))
+                message_id = match_.group(4)
+            else:
+                chat_id = match_.group(2)
+                message_id = match_.group(3)
+            link = [chat_id, int(message_id)]
+        return link_type, link
 
-# def afk_buttons() -> InlineKeyboardMarkup:
-# buttons = [
-# [
-# InlineKeyboardButton(
-# "My Repo", url="https://github.com/samuca78/NoteX"
-# ),
-# InlineKeyboardButton("Github", url="https://github.com"),
-# ],
-# [
-# InlineKeyboardButton("My Git", url="https://github.com/samuca78"),
-# ],
-# ]
-# return InlineKeyboardMarkup(buttons)
+    def afk_buttons() -> InlineKeyboardMarkup:
+       buttons = [
+            [
+                # InlineKeyboardButton(
+                    # "My Repo", url="https://github.com/samuca78/NoteX"
+                # ),
+                InlineKeyboardButton("▫️Bio", url="https://t.me/notezvbio"),
+            ],
+        ]
+        return InlineKeyboardMarkup(buttons)
