5b1b1781683996c2a151e390c73648749d0221a2
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-07-17 21:24:44 -0300

    update afk_inline.py

--- userge/plugins/utils/afk_inline.py
+++ userge/plugins/utils/afk_inline.py
@@ -14,7 +14,8 @@ async def send_inline_afk(message: Message):
 
 async def send_inline_afk_(message: Message):
     bot_ = await userge.bot.get_me()
-    x_ = await userge.get_inline_bot_results(bot_.username, "afk_")
+    # x_ = await userge.get_inline_bot_results(bot_.username, "afk_")
+    x_ = await userge.get_inline_bot_results(bot_.username, "_afk")
     await userge.send_inline_bot_result(
         chat_id=message.chat.id,
         query_id=x_.query_id,
