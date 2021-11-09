2fd4769e45060f129cadf7f8f0afa142bb269ec9
Yusuke <81726687+yusukesy@users.noreply.github.com>
2021-11-09 00:16:16 -0300

    update afk_inline.py

--- userge/plugins/utils/afk_inline.py
+++ userge/plugins/utils/afk_inline.py
@@ -23,7 +23,7 @@ async def send_inline_afk_(message: Message):
     )
 
 
-async def _send_inline_afk(message: Message):
+async def _send_inline_afk_(message: Message):
     _bot = await userge.bot.get_me()
     _x = await userge.get_inline_bot_results(_bot.username, "_afk_")
     await userge.send_inline_bot_result(
